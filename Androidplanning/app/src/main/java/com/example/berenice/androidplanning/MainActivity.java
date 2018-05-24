dee21c4f7e0ce03e0dd6c498ed48d2cec066a2f1
Unknown <till.rexhausen@student.ecp.fr>
2018-05-24 17:45:15 +0200

    Activated name field in main activity

--- Androidplanning/app/src/main/java/com/example/berenice/androidplanning/MainActivity.java
+++ Androidplanning/app/src/main/java/com/example/berenice/androidplanning/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.berenice.androidplanning;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.berenice.androidplanning.database.Staff;
+import com.example.berenice.androidplanning.database.StaffDao;
 import com.example.berenice.androidplanning.database.TestDbActivity;
 import com.example.berenice.androidplanning.records.RecordActivity;
 import com.example.berenice.androidplanning.sendSms.SendSmsActivity;
@@ -20,7 +26,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button nomButton = findViewById(R.id.nameButton);
+        final Button nameButton = (Button) findViewById(R.id.nameButton);
+        nameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Get name from EditText
+                EditText edit = (EditText)findViewById(R.id.nom);
+                String name = edit.getText().toString();
+
+                //Find the data of the staff in the database
+                StaffDao dao = new StaffDao(getBaseContext());
+                dao.open();
+                Staff staff = dao.findStaffByName(name);
+                if (staff == null){
+                    Toast.makeText(MainActivity.this,"Staff not fund",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                int userID = staff.getId();
+
+                //Save id in the preferences
+                SharedPreferences.Editor editor = getSharedPreferences("PlanningPreferences", MODE_PRIVATE).edit();
+                editor.putInt("userID", userID);
+                editor.putString("Day", "4");
+                editor.apply();
+
+                //Open Record activity
+                Intent i = new Intent(getBaseContext(), RecordActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
@@ -57,4 +92,5 @@ public class MainActivity extends AppCompatActivity {
                 return false;
         }
     }
+
 }
