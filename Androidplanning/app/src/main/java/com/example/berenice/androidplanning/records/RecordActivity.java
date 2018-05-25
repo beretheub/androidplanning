f0390b26c464f2e1349f62a3953856123ebb4a19
berenice <berenice.heuberger@student.ecp.fr>
2018-05-25 11:11:50 +0200

    pop-up day

--- Androidplanning/app/src/main/java/com/example/berenice/androidplanning/records/RecordActivity.java
+++ Androidplanning/app/src/main/java/com/example/berenice/androidplanning/records/RecordActivity.java
@@ -1,10 +1,12 @@
 package com.example.berenice.androidplanning.records;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,6 +17,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.example.berenice.androidplanning.R;
@@ -30,6 +33,7 @@ import java.util.ArrayList;
  */
 
 public class RecordActivity extends AppCompatActivity {
+    Button Close;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +59,7 @@ public class RecordActivity extends AppCompatActivity {
         TextView dayTextView = findViewById(R.id.day);
         dayTextView.setText("J" + currentDay);
         TextView nameStaff = findViewById(R.id.nameStaff);
-        nameStaff.setText(currentStaff.getName().toUpperCase() + ", "+ currentStaff.getFirstname());
+        nameStaff.setText(currentStaff.getName().toUpperCase() + ", " + currentStaff.getFirstname());
 
         //generate list
         QueryHandler qh = new QueryHandler();
@@ -68,25 +72,65 @@ public class RecordActivity extends AppCompatActivity {
         ListView lView = (ListView) findViewById(R.id.listTasks);
         lView.setAdapter(adapter);
 
-        TextView day = (TextView) findViewById(R.id.day);
-        day.setOnClickListener(new View.OnClickListener() {
+        Button open =(Button) findViewById(R.id.open_popup);
+        open.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+
+                                    public void onClick(View v) {
+
+                                        showPopup();
+                                    }
+                                }
+                );
+
+/*        TextView create_button = (TextView) findViewById(R.id.day);
+        create_button.setOnClickListener(new View.OnClickListener() {
             @Override
 
             public void onClick(View v) {
 
-                // Réagir au clic
+                showPopup();
 
             }
 
-        });
+        });*/
     }
 
-        @Override
-        public boolean onCreateOptionsMenu (Menu menu){
-            MenuInflater inflater = getMenuInflater();
-            inflater.inflate(R.menu.menu, menu);
-            menu.getItem(1).setVisible(false);
-            return true;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        menu.getItem(1).setVisible(false);
+        return true;
+    }
+
+    ;
+
+
+    private PopupWindow pw;
+
+    private void showPopup() {
+        try {
+// We need to get the instance of the LayoutInflater
+            Context mContext = this.getBaseContext();
+            LayoutInflater inflater = (LayoutInflater)
+                    mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.popup,
+                    (ViewGroup) findViewById(R.id.popup_day));
+            pw = new PopupWindow(layout, 900, 1270, true);
+            pw.showAtLocation(layout, Gravity.CENTER, 0, 0);
+            Close = (Button) layout.findViewById(R.id.close_popup);
+            Close.setOnClickListener(cancel_button);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
+        private View.OnClickListener cancel_button = new View.OnClickListener() {
+            public void onClick(View v) {
+                pw.dismiss();
+            }
+        };
+    }
+
 
