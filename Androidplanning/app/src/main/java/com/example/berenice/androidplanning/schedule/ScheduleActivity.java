a5f9e6fcee34a6e4880aa22e8a69472af81b1b26
Unknown <till.rexhausen@student.ecp.fr>
2018-05-27 17:32:41 +0200

    Pop up day matching to the day

--- Androidplanning/app/src/main/java/com/example/berenice/androidplanning/schedule/ScheduleActivity.java
+++ Androidplanning/app/src/main/java/com/example/berenice/androidplanning/schedule/ScheduleActivity.java
@@ -23,6 +23,7 @@ import android.widget.Button;
 
 
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.PopupWindow;
 
@@ -36,6 +37,11 @@ import com.example.berenice.androidplanning.database.StaffDao;
 import com.example.berenice.androidplanning.database.Task;
 import com.example.berenice.androidplanning.task.taskActivity;
 
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -51,14 +57,15 @@ public class ScheduleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
         //Access shared preferences
         SharedPreferences prefs = getSharedPreferences("PlanningPreferences", MODE_PRIVATE);
         int userID = prefs.getInt("userID", 0);
         String currentDay = prefs.getString("Day", "1");
 
-        toolbar.setTitle(R.id.nameTask_schedule+ " J" + currentDay );
+        toolbar.setTitle(" Schedule J" + currentDay );
+        setSupportActionBar(toolbar);
 
         //find current Staff if there is one
         StaffDao dao = new StaffDao(this);
@@ -124,8 +131,64 @@ public class ScheduleActivity extends AppCompatActivity {
                     mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
             View layout = inflater.inflate(R.layout.popup_day,
                     (ViewGroup) findViewById(R.id.popup_day));
+
+            //Set content
+            SharedPreferences prefs =
+                    getSharedPreferences("PlanningPreferences", MODE_PRIVATE);
+            ImageView profileView = (ImageView)layout.findViewById(R.id.profil);
+            String currentDay= prefs.getString("Day","1");
+            switch (currentDay) {
+                case "1":
+                    profileView.setImageResource(R.drawable.profil_1);
+                    break;
+                case "2":
+                    profileView.setImageResource(R.drawable.profil_2);
+                    break;
+                case "3":
+                    profileView.setImageResource(R.drawable.profil_3);
+                    break;
+                case "4":
+                    profileView.setImageResource(R.drawable.profil_4);
+                    break;
+                case "5":
+                    profileView.setImageResource(R.drawable.profil_5);
+                    break;
+            }
+
+            //Read information of the day
+            InputStreamReader is = new InputStreamReader(getBaseContext().getAssets()
+                    .open("planningDB/J" + currentDay + "/infos.txt"), StandardCharsets.ISO_8859_1);
+
+            BufferedReader reader = new BufferedReader(is);
+            int i = 0;
+            String line;
+            String text[] = new String[6];
+            while ((line = reader.readLine())!= null){
+                text[i] = line;
+                i++;
+            }
+
+            reader.close();
+            is.close();
+
+            TextView currentText = (TextView) layout.findViewById(R.id.popup_line1);
+            currentText.setText(text[0]);
+            currentText = (TextView) layout.findViewById(R.id.popup_line2);
+            currentText.setText(text[1]);
+            currentText = (TextView) layout.findViewById(R.id.popup_line3);
+            currentText.setText(text[2]);
+            currentText = (TextView) layout.findViewById(R.id.popup_line4);
+            currentText.setText(text[3]);
+            currentText = (TextView) layout.findViewById(R.id.popup_line5);
+            currentText.setText(text[4]);
+            currentText = (TextView) layout.findViewById(R.id.popup_line6);
+            currentText.setText(text[5]);
+
             pw = new PopupWindow(layout, 900, 1270, true);
             pw.showAtLocation(layout, Gravity.CENTER, 0, 0);
+
+
+
             Close = (Button) layout.findViewById(R.id.close_popup);
             Close.setOnClickListener(cancel_button);
         } catch (Exception e) {
